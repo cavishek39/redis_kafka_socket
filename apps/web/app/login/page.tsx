bbe278b242792167374ec6be8a6e918d03a8ea4b
dev-avishek <avishekchatterjee@tractorjunction.com>
2023-12-24 22:54:17 +0530

    clerk auth added

---
+++ apps/web/app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+function Login() {
+  return (
+    <div className=''>
+      <input placeholder='Email' />
+    </div>
+  )
+}
+
+export default Login
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react'
 import { useSocket } from '../context/SocketProvider'
 import classes from './page.module.css'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Page() {
   const { sendMessage, messages } = useSocket()
@@ -17,6 +18,7 @@ export default function Page() {
           alignItems: 'center',
           justifyContent: 'center',
         }}>
+        <UserButton />
         <input
           onChange={(e) => setMessage(e.target.value)}
           className={classes['chat-input']}
